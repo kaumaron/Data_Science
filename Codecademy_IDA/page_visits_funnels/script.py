b1c32aaa558857cdf86f9ade67b758bf588f30de
Andrew K DeCotiis-Mauro <andrewkmauro@gmail.com>
2018-05-03 12:00:29 -0400

    added in calculations for unique values

--- Codecademy_IDA/page_visits_funnels/script.py
+++ Codecademy_IDA/page_visits_funnels/script.py
@@ -19,7 +19,7 @@ purchase = pd.read_csv('purchase.csv',
 #print(purchase.head())
 
 #df.shape[0] is used in favor of len(df) for speed purposes. It doesn't matter
-# for this exercise but on a large dataset it would. The zero index is due to the 
+# for this exercise but on a large dataset it would. The zero index is due to
 # shape being a tuple: (rows, columns).
 
 #Left merge visits and carts. Find number of rows in merged DF
@@ -32,12 +32,13 @@ print('Visits but no cart: {}'.format(no_cart_time))
 
 #calculate percentage of visitors *not* adding to cart
 percent_no_tshirt = no_cart_time * 100 / visit_carts.shape[0]
-print('Percent of visitors without adding to cart: {:.2f}%'.format(percent_no_tshirt))
+print('Percent of visitors without adding to cart: {:.2f}%'\
+    .format(percent_no_tshirt))
 
 #Left merge cart and checkout DFs. Determine percent of abandoned carts.
 cart_checkout = cart.merge(checkout, how = 'left')
-no_checkout = cart_checkout.checkout_time[cart_checkout.checkout_time.isnull()].shape[0]\
- * 100 / cart_checkout.shape[0]
+no_checkout = cart_checkout.checkout_time[cart_checkout.checkout_time\
+    .isnull()].shape[0] * 100 / cart_checkout.shape[0]
 print('Percent of carts that do not checkout: {:.2f}%'.format(no_checkout))
 
 #Left merge all data into large DF
@@ -49,8 +50,9 @@ all_data = visits.merge(cart, how = 'left')\
 #Completely ignore all_data and left merge checkout and purchase DFs 
 # to find the abandoned checkouts
 checkout_purchase = checkout.merge(purchase, how =  'left')
-percent_checkout_no_purchase = checkout_purchase[checkout_purchase.purchase_time.isnull()]\
-.shape[0] * 100 / checkout_purchase[checkout_purchase.checkout_time.notnull()].shape[0]
+percent_checkout_no_purchase = checkout_purchase[checkout_purchase\
+    .purchase_time.isnull()].shape[0] * 100 / \
+    checkout_purchase[checkout_purchase.checkout_time.notnull()].shape[0]
 print('Percent of users that leave checkout without purchase: {:.2f}%'\
 .format(percent_checkout_no_purchase))
 
@@ -62,4 +64,28 @@ all_data['time_to_purchase'] = all_data.purchase_time\
 print('Average time to purchase: {}'\
       .format(all_data.time_to_purchase.mean()))
 
+#
+#Recalculate using only all_data and unique visitors
+#
+print('\nThe all_data DF has {} entries.'.format(
+                                all_data.user_id.unique().shape[0])
+                                )
 
+no_cart_time_2 = all_data[all_data.cart_time.isnull()].user_id.unique()\
+            .shape[0]
+percent_no_tshirt_2 = no_cart_time * 100 / all_data[all_data\
+    .visit_time.notnull()].user_id.unique().shape[0]
+print('*Percent of visitors without adding to cart: {:.2f}%'\
+    .format(percent_no_tshirt_2))
+
+no_checkout_2 = all_data[all_data.checkout_time.isnull() & all_data.cart_time\
+    .notnull()].user_id.unique().shape[0] * 100 /\
+    all_data[all_data.cart_time.notnull()].user_id.unique().shape[0]
+print('*Percent of carts that do not checkout: {:.2f}%'.format(no_checkout_2))
+
+percent_checkout_np_2 = all_data[all_data.purchase_time.isnull() & all_data\
+    .checkout_time.notnull()].user_id.unique().shape[0] /\
+        all_data[all_data.checkout_time.notnull()].user_id.unique().shape[0] \
+        * 100
+print('*Percent of users that leave checkout without purchase: {:.2f}%'\
+    .format(percent_checkout_np_2))
