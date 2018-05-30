640c4111f6565b6e002af1d0a2e94fcc4afbd53e
Andrew K DeCotiis-Mauro <andrewkmauro@gmail.com>
2018-05-30 00:41:23 -0400

    updated Fetchmaker script

--- Codecademy_IDA/FetchMaker/script.py
+++ Codecademy_IDA/FetchMaker/script.py
@@ -1,6 +1,6 @@
 import numpy as np
 import fetchmaker
-from scipy.stats import binom_test, f_oneway
+from scipy.stats import binom_test, f_oneway, chi2_contingency
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 # Testing out the functions
@@ -32,13 +32,39 @@ print('Reject the null hypothesis that whippets, terriers and pitbulls have the 
 
 # Determine which breed is different
 labels =  ['whippet']*len(weights[0]) + \
-					['terrier']*len(weights[1]) + \
-					['pitbull']*len(weights[2])
-print(pairwise_tukeyhsd(np.concatenate(weights),
+            ['terrier']*len(weights[1]) + \
+            ['pitbull']*len(weights[2])
+
+tukey_range = pairwise_tukeyhsd(np.concatenate(weights),
                        labels,
-                         0.05))
-
-
+                         0.05)
+count = 0
+for i in range(len(tukey_range.groupsunique)-1):
+    for j in range(i+1, len(tukey_range.groupsunique)):
+      print('Reject {}-{}? {}'.format(
+      tukey_range.groupsunique[i],
+      tukey_range.groupsunique[j],
+      tukey_range.reject[count]
+    	))
+      count += 1
+print('\n')
 # Terrier is significantly different
 for key, breed in enumerate(['whippet', 'terrier','pitbull']):
-    print('{}: {}'.format(breed, np.mean(weights[key])))
+    print('{}: {} +/- {:.2f}'.format(
+      breed,
+      np.mean(weights[key]),
+      np.std(weights[key])
+    ))
+
+poodle_colors = fetchmaker.get_color('poodle')
+shihtzu_colors = fetchmaker.get_color('shihtzu')
+
+colors = ['black', 'brown', 'gold', 'grey', 'white']
+color_table = np.array(
+    [[np.count_nonzero(poodle_colors == color) for color in colors],
+    [np.count_nonzero(shihtzu_colors == color) for color in colors]]).transpose()
+
+print('\n The p value for the hypothesis that poodles and shihtzus have' +
+        ' significantly different color breakdowns is {:.4f} '.format(
+        chi2_contingency(color_table)[1]
+        ))
